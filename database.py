71df254cd4c7058689f2d7b3616d809a58a65cc5
Valentina Restrepo <vrestrepomontoya@gmail.com>
2025-05-02 16:15:38 -0500

    delete connection bd

--- database.py
+++
@@ -1,4 +0,0 @@
-from databases import Database
-
-DATABASE_URL = "postgresql://postgres:Valen.1705@db.eodddzigjyhcxytfvfoz.supabase.co:5432/postgres"
-database = Database(DATABASE_URL)
--- main.py
+++ main.py
@@ -3,17 +3,9 @@ from models import loan_products
 from schemas import LoanProductCreate, LoanSimulationInput, LoanSimulationResult
 from utils import calculate_monthly_payment
 from fastapi.middleware.cors import CORSMiddleware
-from database import database
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup():
-    await database.connect()
-
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
 
 @app.get("/")
 def read_root():

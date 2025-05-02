7113571a5dc5a57495d8615a8bf7c6a949b0bb5e
Valentina Restrepo <vrestrepomontoya@gmail.com>
2025-05-02 16:06:21 -0500

    add requirements.txt

---
+++ database.py
@@ -0,0 +1,4 @@
+from databases import Database
+
+DATABASE_URL = "postgresql://postgres:Valen.1705@db.eodddzigjyhcxytfvfoz.supabase.co:5432/postgres"
+database = Database(DATABASE_URL)
--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from models import loan_products
 from schemas import LoanProductCreate, LoanSimulationInput, LoanSimulationResult
 from utils import calculate_monthly_payment
 from fastapi.middleware.cors import CORSMiddleware
+from database import database
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 @app.get("/")
 def read_root():
